4125180f2ad66a69f53120b4cda2ee31c3433e58
Xiandec <timurgalimov050722@gmail.com>
2023-09-19 18:33:46 +0300

    4 task and fix 3 task

--- fast_3.py
+++ fast_3.py
@@ -4,11 +4,10 @@ def dict_1():
     list_2 = input('List 2: ').split()
     if len(list_1) == len(list_2):
         dictionary = {k:v for k, v in zip(list_1, list_2) if list_1.count(k) == 1}
-        return dictionary
+        print(dictionary)
     else:
         print('Ошибка')
-    return False
 
 def dict_2():
     dictionary = {i:i**3 for i in range(1, 11)}
-    return dictionary
+    print(dictionary)
---
+++ fast_4.py
@@ -0,0 +1,23 @@
+def check_any_sumb_in_str():
+    string = input('Строка: ')
+    if len(string) != 0:
+        print('Симовлы есть')
+    else:
+        print('Символов нет')
+
+def ch_nch_min_nch():
+    n = int(input('n: '))
+    ch = []
+    nch = []
+    for i in range(n):
+        a = int(input(str(i) + ': '))
+        if a % 2 == 0:
+            ch.append(a)
+        else:
+            nch.append(a)
+    print(ch, nch, min(nch), sep='\n')
+
+def dictionary_month():
+    month = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь']
+    dict_month = {k: v for k, v in zip(month, range(1, 13))}
+    print(dict_month)
