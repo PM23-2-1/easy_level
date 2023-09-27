88786db755dea3343438b426f979ca82bd3d8fb6
Xiandec <timurgalimov050722@gmail.com>
2023-09-27 10:56:01 +0300

    6 extend

--- fast_6.py
+++ fast_6.py
@@ -1,3 +1,9 @@
 def list_120():
     list_1 = [i for i in range(120)]
     print(len(list_1), sum(list_1), sum(list_1) / len(list_1), sep='\n')
+
+def generate_list_120():
+    return [i for i in range(120)]
+
+def len_sum_list(list_1):
+    print(len(list_1), sum(list_1), sum(list_1) / len(list_1), sep='\n')
